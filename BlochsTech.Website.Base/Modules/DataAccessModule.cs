836e694799c653042aa17ad0b15b6fa347401c1b
AlexOliinyk1 <alex.oliinyk1@gmail.com>
2015-01-09 18:20:01 +0200

    Configuration Autofac.

---
+++ BlochsTech.Website.Base/Modules/DataAccessModule.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using System.Reflection;
+using Autofac;
+using BlochsTech.Website.DataAccess.Infrastructure;
+
+namespace BlochsTech.Website.Base.Modules
+{
+    public class DataAccessModule: Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(Assembly.Load("BlochsTech.Website.DataAccess"))
+                   .Where(t => t.Name.EndsWith("Repository"))
+                   .AsImplementedInterfaces()
+                  .InstancePerLifetimeScope();
+        }
+        
+        
+     
+}
---
+++ BlochsTech.Website.Base/Modules/ServiceModule.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Autofac;
+
+namespace BlochsTech.Website.Base.Modules
+{
+    public class ServiceModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(Assembly.Load("BlochsTech.Website.Service"))
+                .Where(t => t.Name.EndsWith("Service"))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+        }
+
+    }
+}
